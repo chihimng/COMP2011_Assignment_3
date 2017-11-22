8a6b0164e7fc441719d117b090a1eae02c43fbaf
Elliot Ng <chihimng.io@gmail.com>
2017-11-22 22:21:54 +0800

    init commit

---
+++ COMP2011_Assignment_3/pa3.cpp
@@ -0,0 +1,150 @@
+/*
+ *       File: pa3.cpp
+ * Created on: Nov 1, 2017
+ *     Author: Yao Yao (yyaoag@cse.ust.hk)
+ *       Note: Implement all functions without changing any function prototype.
+ *             This is the only file you should modify and submit.
+ */
+
+#include <iostream>
+#include "structure.h"
+using namespace std;
+
+/*
+ * Description: get the pointer to the frame through frame index.
+ *
+ *       video: input video structure.
+ * frame_index: the index of the frame to be extracted.
+ *      return: the pointer to the frame or nullptr if the frame does not exsist.
+ */
+Frame * GetFrame(const Video & video, const int frame_index)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: get the pointer to the vehicle through vehicle index.
+ *
+ *         video: input video structure.
+ * vehicle_index: the index of the vehicle to be extracted.
+ *        return: the pointer to the vehicle or nullptr if the vehicle does not exsist.
+ */
+Vehicle * GetVehicle(const Video & video, const int vehicle_index)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: get the pointer to vf_info of a vehicle in one frame.
+ *
+ *     vehicle: input vehicle structure.
+ * frame_index: the frame index of the vf_info to be extracted from this vehicle.
+ *      return: the pointer to the vf_info or nullptr if the vehicle does not visible in
+ *              this frame.
+ */
+VehicleFrameInfo * GetVFInfo(const Vehicle * vehicle, const int frame_index)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: initialize a new frame in the video during frame processing. You need to
+ *              create a new frame, and initialize this new frame and add it to the link
+ *              list. In this function, you don't need to use hard copy to initialize
+ *              Frame.image. Also, you don't need to analyze the vehicles in this frame
+ *              (set num_vehicles of this new frame as 0 and all pointers to vehicles as
+ *              nullptr).
+ *
+ *  video: input video structure.
+ * return: true if finish and false if all frames has been processed.
+ */
+bool InitializeNewFrame(Video & video)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: add one vf_info to the video. You need to check if the vf_info, the
+ *              corresponding video and the corresponding the vehicle has been in the
+ *              video before you add the VFInfo.
+ *
+ *              video: input video structure.
+ * vehicle_frame_info: pointer to the new info to be added. (vf_info)
+ *             return: return true if finish and false if
+ *                     1. the frame or the vehicle of this vf_info is not in this video or
+ *                     2. vf_info has already been in the list of the vehicle.
+ */
+bool AddVFInfo(Video & video, VehicleFrameInfo * vehicle_frame_info)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: track the vehicle in the new frame and return the new info. The speed
+ *              is the distance between positions of the vehicle in two frames and note
+ *              that 1. the maximum speed of one vehicle is MAX_SPEED and 2. the minimum
+ *              distance between vehicles in the same lane is MAX_SPEED and 3. the vehicle
+ *              is assumed always to stay in the same lane.
+ *       vehicle: pointer to the vehicle to be tracked.
+ * current_frame: pointer to the frame the vehicle to be tracked in.
+ *    prev_frame: pointer to the previous frame the vehicle was in.
+ *        return: the pointer to the new info or nullptr if 1. the vehicle is not in
+ *                prev_frame or 2. the vehicle left scope of current frame.
+ */
+VehicleFrameInfo * TrackVehicle(const Vehicle * vehicle, const Frame * current_frame, const Frame * prev_frame)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: find and add new vehicles in the last frame of the frame list. You need to 1. find the
+ *              new vehicles in the first colume of each lane, and 2. create and initialize a new vehicle
+ *              is a new vehicle is found and, 3. add the new vehicle to the video and, 4. create a
+ *              vf_info for the new vehicle in this frame and, 5. add the vf_info to the video.
+ *
+ *  video: input video structure.
+ * return: true is finish or false if the frame index is invalid.
+ */
+bool FindAndAddNewVehicles(Video & video)
+{
+	// your implementation
+
+	// Hints:
+	// detect new vehicles lane by lane
+
+		// check if there is a new vehicle in the lane
+
+		// construct and add a new vehicle
+
+		// construct and add a new vf_info
+}
+
+/*
+ * Description: calcute the average speed of all vehicles in all their visible frames (the average of all speeds).
+ *
+ *  video: input video structure.
+ * return: the average speed or 0 if no vehicle is in the video.
+ */
+double AverageRoadSpeed(Video & video)
+{
+	// your implementation
+
+}
+
+/*
+ * Description: clean all memories of the video, including raw_data, all frames, vehicles and vf_infos.
+ *
+ *  video: input video to be clean.
+ * return: no return.
+ */
+void CleanVideo(Video & video)
+{
+	// your implementation
+
+}
